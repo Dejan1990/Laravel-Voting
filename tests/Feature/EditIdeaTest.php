f9ecbeeb40c3076cd82b070d569514adc19d15c3
Dejan <38137502+Dejan1990@users.noreply.github.com>
2021-10-21 15:09:42 +0200

    Delete Idea Test & Fix Edit Idea Test Errors

--- tests/Feature/EditIdeaTest.php
+++ tests/Feature/EditIdeaTest.php
@@ -2,23 +2,23 @@
 
 namespace Tests\Feature;
 
-use App\Http\Livewire\EditIdea;
-use App\Http\Livewire\IdeaShow;
-use App\Models\Category;
+use Tests\TestCase;
 use App\Models\Idea;
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Http\Response;
 use Livewire\Livewire;
-use Tests\TestCase;
+use App\Models\Category;
+use Illuminate\Http\Response;
+use App\Http\Livewire\EditIdea;
+use App\Http\Livewire\IdeaShow;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class EditIdeaTest extends TestCase
 {
     use RefreshDatabase;
 
     /** @test */
-   /* public function shows_edit_idea_livewire_component_when_user_has_authorization()
+    public function shows_edit_idea_livewire_component_when_user_has_authorization()
     {
         $user = User::factory()->create();
         $idea = Idea::factory()->create([
@@ -28,7 +28,7 @@ class EditIdeaTest extends TestCase
         $this->actingAs($user)
             ->get(route('idea.show', $idea))
             ->assertSeeLivewire('edit-idea');
-    }*/
+    }
 
     /** @test */
     public function does_not_show_edit_idea_livewire_component_when_user_does_not_have_authorization()
@@ -42,7 +42,7 @@ class EditIdeaTest extends TestCase
     }
 
     /** @test */
-   /* public function edit_idea_form_validation_works()
+    public function edit_idea_form_validation_works()
     {
         $user = User::factory()->create();
         $idea = Idea::factory()->create([
@@ -59,10 +59,10 @@ class EditIdeaTest extends TestCase
             ->call('updateIdea')
             ->assertHasErrors(['title', 'category', 'description'])
             ->assertSee('The title field is required');
-    }*/
+    }
 
     /** @test */
-    /*public function editing_an_idea_works_when_user_has_authorization()
+    public function editing_an_idea_works_when_user_has_authorization()
     {
         $user = User::factory()->create();
 
@@ -89,35 +89,35 @@ class EditIdeaTest extends TestCase
             'description' => 'This is my edited idea',
             'category_id' => $categoryTwo->id,
         ]);
-    }*/
+    }
 
-     /** @test */
-     public function editing_an_idea_does_not_work_when_user_does_not_have_authorization_because_different_user_created_idea()
-     {
-         $user = User::factory()->create();
-         $userB = User::factory()->create();
- 
-         $categoryOne = Category::factory()->create(['name' => 'Category 1']);
-         $categoryTwo = Category::factory()->create(['name' => 'Category 2']);
- 
-         $idea = Idea::factory()->create([
-             'user_id' => $user->id,
-             'category_id' => $categoryOne,
-         ]);
- 
-         Livewire::actingAs($userB)
-             ->test(EditIdea::class, [
-                 'idea' => $idea,
-             ])
-             ->set('title', 'My Edited Idea')
-             ->set('category', $categoryTwo->id)
-             ->set('description', 'This is my edited idea')
-             ->call('updateIdea')
-             ->assertStatus(Response::HTTP_FORBIDDEN);
-     }
+    /** @test */
+    public function editing_an_idea_does_not_work_when_user_does_not_have_authorization_because_different_user_created_idea()
+    {
+        $user = User::factory()->create();
+        $userB = User::factory()->create();
 
-     /** @test */
-   /* public function editing_an_idea_does_not_work_when_user_does_not_have_authorization_because_idea_was_created_longer_than_an_hour_ago()
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+        $categoryTwo = Category::factory()->create(['name' => 'Category 2']);
+
+        $idea = Idea::factory()->create([
+            'user_id' => $user->id,
+            'category_id' => $categoryOne,
+        ]);
+
+        Livewire::actingAs($userB)
+            ->test(EditIdea::class, [
+                'idea' => $idea,
+            ])
+            ->set('title', 'My Edited Idea')
+            ->set('category', $categoryTwo->id)
+            ->set('description', 'This is my edited idea')
+            ->call('updateIdea')
+            ->assertStatus(Response::HTTP_FORBIDDEN);
+    }
+
+    /** @test */
+    /*public function editing_an_idea_does_not_work_when_user_does_not_have_authorization_because_idea_was_created_longer_than_an_hour_ago()
     {
         $user = User::factory()->create();
 
@@ -142,7 +142,7 @@ class EditIdeaTest extends TestCase
     }*/
 
     /** @test */
-   /* public function editing_an_idea_shows_on_menu_when_user_has_authorization()
+    public function editing_an_idea_shows_on_menu_when_user_has_authorization()
     {
         $user = User::factory()->create();
 
@@ -156,7 +156,7 @@ class EditIdeaTest extends TestCase
                 'votesCount' => 4,
             ])
             ->assertSee('Edit Idea');
-    }*/
+    }
 
     /** @test */
     public function editing_an_idea_does_not_show_on_menu_when_user_does_not_have_authorization()
